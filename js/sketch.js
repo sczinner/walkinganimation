54fff6c16d9a6287977d64d3176a28fa8b92491b
sczinner <46569422+sczinner@users.noreply.github.com>
2021-02-20 19:25:52 -0500

    Complete stick figure walking

--- js/sketch.js
+++ js/sketch.js
@@ -67,11 +67,22 @@ class Sketch {
     var floor={id:3, x1:0, y1:this.floorheight,
           x2:this.ctx.canvas.width,y2:this.floorheight,colour:"white"};
 
-    //var bodylength=100;
-    //var body={id:4, x1:this.startx, y1:this.floorheight-this.leglength-bodylength,
-      //        x2:this.startx,y2:this.floorheight-this.leglength,colour:"green"};
+    this.bodylength=100;
+    var body={id:4, x1:this.startx, y1:this.floorheight-this.leglength-this.bodylength,
+             x2:this.startx,y2:this.floorheight-this.leglength,colour:"green"};
 
-    this.sticks = {floor:floor,leg1:leg1, leg2:leg2}
+  this.armheight=75;//height from top of leg
+  this.armlength=75;
+   var arm1={id:5, x1:this.startx, y1:this.floorheight-this.leglength-this.armheight-this.armlength,
+      x2:this.startx,y2:this.floorheight-this.armheight,colour:"red"};
+      var arm2={id:6, x1:this.startx, y1:this.floorheight-this.leglength-this.armheight-this.armlength,
+         x2:this.startx,y2:this.floorheight-this.armheight,colour:"blue"};
+
+    this.headsize = 30;
+    this.head = {id:7, x:this.startx,
+      y:this.floorheight-this.leglength-this.bodylength-this.headsize,r:this.headsize,colour:"purple"}
+
+    this.sticks = {floor:floor,leg1:leg1, leg2:leg2, body:body,arm1:arm1,arm2:arm2}
 
     this.leg1onground=true;
     this.legangle=0;///from -vertical (because +y is down)
@@ -89,12 +100,17 @@ class Sketch {
     for(var propt in this.sticks){
       this.drawStick(this.sticks[propt]);
     }
+    //draw head
+    this.solidCirc(this.head.x,this.head.y,this.head.r,this.head.colour)
 
-    this.moveLegs()
+
+    this.moveLegs();
+    this.moveBody();
+    this.moveArms();
+    this.moveHead();
 
     if(this.sticks["leg1"].x1>this.ctx.canvas.width||this.sticks["leg1"].x1<0){
       this.changeDirection();
-
     }
 
     }
@@ -108,7 +124,39 @@ class Sketch {
   drawStick(stick){
     this.drawLine(stick.x1,stick.y1,stick.x2,stick.y2,stick.colour);
   }
+  moveHead(){
+    this.head.x=this.sticks['leg1'].x1;
 
+    this.head.y=this.sticks['leg1'].y1-this.bodylength-this.headsize;
+  }
+
+  moveArms(){
+    //move arm opposite leg etc
+    this.sticks['arm1'].x1=this.sticks['leg1'].x1;
+    this.sticks['arm2'].x1=this.sticks['leg1'].x1;
+
+    this.sticks['arm1'].y1=this.sticks['leg1'].y1-this.armheight;
+    this.sticks['arm2'].y1=this.sticks['leg1'].y1-this.armheight;
+
+    var armtolegratio = this.armlength/this.leglength
+
+    var dx1 = (this.sticks['leg2'].x2-this.sticks['leg2'].x1)*armtolegratio
+    var dy1 = (this.sticks['leg2'].y2-this.sticks['leg2'].y1)*armtolegratio
+    this.sticks['arm1'].x2=this.sticks['arm1'].x1+dx1;
+    this.sticks['arm1'].y2=this.sticks['arm1'].y1+dy1;;
+
+    var dx2 = (this.sticks['leg1'].x2-this.sticks['leg1'].x1)*armtolegratio
+    var dy2 = (this.sticks['leg1'].y2-this.sticks['leg1'].y1)*armtolegratio
+    this.sticks['arm2'].x2=this.sticks['arm2'].x1+dx2;
+    this.sticks['arm2'].y2=this.sticks['arm2'].y1+dy2;;
+  }
+  moveBody(){
+    this.sticks['body'].x1=this.sticks['leg1'].x1;
+    this.sticks['body'].x2=this.sticks['leg1'].x1;
+
+    this.sticks['body'].y1=this.sticks['leg1'].y1-this.bodylength;
+    this.sticks['body'].y2=this.sticks['leg1'].y1;
+  }
   moveLegs(){
     if(this.legangle<this.maxlegangle){
       this.legangle+=this.leganglespeed;
